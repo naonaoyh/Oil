b9896ad63231ff9b39696973a31010c478c973ab
garymawdsley <garymawdsley@googlemail.com>
2009-06-25 15:54:43 +0100

    removed recently added helper methods - no longer required

--- lib/Element.rb
+++ lib/Element.rb
@@ -8,27 +8,5 @@ class Element
     @parent = parent
     @fields = []
     @type = type
-  end
-
-  def info
-    puts "@coverage=#{@coverage},@name=#{@name},@type=#{@type},PARENT=#{parent},CHILDREN=#{offspring}"
-  end
-
-  def succinct_info
-    "@name=#{@name}"
-  end
-  
-  def parent
-    return @parent.info if @parent
-  end
-  
-  def offspring
-    childrenStr=""
-    if (@children and @children.length > 0)
-      @children.each do |c|
-        childrenStr += "<#{c.succinct_info}>"
-      end
-    end
-    childrenStr
-  end
+  end  
 end
